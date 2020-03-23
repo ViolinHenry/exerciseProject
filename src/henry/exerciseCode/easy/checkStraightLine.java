77afd4a826f2fcebc284377a9404b90a3f1e0553
hehaoyuan <hehaoyuan@dashuf.com>
2020-03-23 15:51:29 +0800

    20200323

--- src/henry/exerciseCode/easy/checkStraightLine.java
+++ src/henry/exerciseCode/easy/checkStraightLine.java
@@ -9,7 +9,8 @@ package easy;
 
 /**
  * @Title:
- * @Description: 在一个 XY 坐标系中有一些点，我们用数组 coordinates 来分别记录它们的坐标，其中 coordinates[i] = [x, y] 表示横坐标为 x、纵坐标为 y 的点。请你来判断，这些点是否在该坐标系中属于同一条直线上，是则返回 true，否则请返回 false。
+ * @Description: 在一个 XY 坐标系中有一些点，我们用数组 coordinates 来分别记录它们的坐标，其中 coordinates[i] = [x, y] 表示横坐标为 x、纵坐标为 y 的点。
+ * 请你来判断，这些点是否在该坐标系中属于同一条直线上，是则返回 true，否则请返回 false。
  * @author: hehaoyuan
  * @param: ${TAGS}      
  * @return: ${RETURN_TPYE}
@@ -18,7 +19,7 @@ package easy;
  */
 public class checkStraightLine {
     public boolean checkStraightLine(int[][] coordinates) { //运用斜率方程结题
-        int x1 = coordinates[1][0] - coordinates[0][0];
+        int x1 = coordinates[1][0] - coordinates[0][0]; //k = y2 - y1 / x2 - x1
         int y1 = coordinates[1][1] - coordinates[0][1];
         for (int i = 2; i < coordinates.length; i++) {
             int x2 = coordinates[i][0] - coordinates[0][0];
